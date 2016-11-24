64b555813bc162a9e522924fa050fac9eb1b51de
J Chen <jjccforth@gmail.com>
2016-11-23 20:19:51 -0500

    init commit

---
+++ BizExcel.py
@@ -0,0 +1,51 @@
+
+import xlwings as xw
+import os
+
+income_tag = "PAYBACK" #Income for bank
+
+#D:/jchen/Documents/Biz/TDBizOct2015-Oct2016.xlsx
+bk = xw.books[0]
+st = bk.sheets[0] # 0 for bank, 2 for visa
+rng = xw.books[0].sheets[2].range('A1:H250')
+dic = {}
+count = 0
+for r in rng.rows:
+    (date,content, amountl, amountr,code) = (r[1].value,r[2].value,r[3].value,r[4].value, r[6].value)
+    if (date == None):
+        continue
+    (m,d,y) = date.split('/')
+    count +=1
+    print count
+    key = m + "-" + y
+
+    if not key in dic:
+        dic[key] = {}
+    if not code in dic[key]:
+        dic[key][code] = 0
+
+    #if ( code == "Income"):
+    if (code == income_tag):
+        dic[key][code] += amountr
+    else:
+        if(amountl == None): #refund
+            continue
+        dic[key][code] += amountl
+
+    #print date, content,amount
+    (m,d,y) = date.split('/')
+    #print '{0}, {1},{2}'.format(m , content, amountl)
+total = 0
+
+
+for my, v in sorted(dic.items()):
+    for code in v:
+        if income_tag == code:
+            print "Income:\t{}\t\t{}".format(my, v[income_tag])
+            total += v[income_tag]
+        else:
+            print "Expense:\t{}\t{}\t{}".format(my, code, v[code])
+    print "\n"
+
+
+print total
