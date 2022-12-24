4744a54c0464d87594a56074c254afc481c02d9a
jjcc <jjccforth@gmail.com>
2022-12-24 13:23:32 -0500

    mod

--- BookKeepying.py
+++ BookKeepying.py
@@ -1,13 +1,21 @@
 import pandas as pd
+import dotenv
+import os
 
-data_dir = "data4bkping/"
-visa_fn = "TD_VISA_1747_Only.xlsx"
+# Load .env file
+dotenv.load_dotenv()
+
+# Get the data directory from the .env file
+data_dir = os.getenv("DATA_DIR")
+
+# Get the file name from the .env file
+visa_fn = os.getenv("BANK_STMTS")
 
 
-vxls_file = pd.ExcelFile(data_dir + visa_fn)
+vxls_file = pd.ExcelFile(f"{data_dir}/{visa_fn}")
+#vxls_file = pd.read_excel(f"{data_dir}/{visa_fn}")
 
 print(vxls_file.sheet_names)
 
-df = vxls_file.parse('Sheet1')
+#df = vxls_file.parse('Sheet1')
 
-#df
